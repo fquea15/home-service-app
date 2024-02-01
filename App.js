51265b4eb4f92a21c79c802c08bcc497298159c7
fquea15 <qjcoders@gmail.com>
2024-02-01 05:41:25 -0500

    Authentication

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './App/Screens/LoginScreen/Login';
+import { ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-expo';
+import * as SecureStore from "expo-secure-store";
 
+const tokenCache = {
+  async getToken(key) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key, value) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ClerkProvider
+      tokenCache={tokenCache}
+      publishableKey= "CLERK_PUBLISHABLE_KEY">
+      <View style={styles.container}>
+        {/* Sign In Component */}
+        <SignedIn>
+          <Text>You are Signed in</Text>
+        </SignedIn>
+
+        {/* SignOut */}
+        <SignedOut>
+          <Login />
+        </SignedOut>
+      </View>
+    </ClerkProvider>
   );
 }
 
@@ -14,7 +44,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 20,
   },
 });
